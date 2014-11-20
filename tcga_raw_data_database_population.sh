1a4f2c73cdfd3b82b0c84bf83b7c98727c0e01a8
Izaskun Mallona <imallona@imppc.org>
2014-11-20 12:31:45 +0100

    Intermediate

--- tcga_raw_data_database_population.sh
+++ tcga_raw_data_database_population.sh
@@ -20,7 +20,10 @@ PASS=""
 
 # data origins stuff
 WDIR=/imppc/labs/maplab/share/anna2izaskun/db_region_profile_data
-DAT=(BLCA BRCA CESC COAD GBM  HNSC KIRC KIRP LIHC LUAD LUSC PAAD PCPG PRAD READ SARC SKCM THCA UCEC)
+# DAT=(BLCA BRCA CESC COAD GBM  HNSC KIRC KIRP LIHC LUAD LUSC PAAD PCPG PRAD READ SARC SKCM THCA UCEC)
+# some of them are already in xd
+DAT=(BLCA CESC GBM HNSC KIRC KIRP LIHC LUSC PAAD PCPG PRAD READ SARC SKCM UCEC)
+
 
 PLATFORM=(450KMeth RNAseq)
 CTYPE=(Normal Tumor)
@@ -37,12 +40,14 @@ do
     do
         for ctype in ${CTYPE[@]}
         do
-            curr_data_fn="$name"/"$type"/"$name"_"$platform"_"$ctype".csv
+            curr_data_fn="$WDIR"/"$name"/"$platform"/"$ctype"/"$name"_"$platform"_"$ctype".csv
             curr_sql_fn="$WDIR"/"$name"/"$platform"/"$ctype"/"$name"_"$platform"_"$ctype"_createTable.txt
 
             # fixing the table declarations start
-
             awk '{if (NR > 1) print}' "$curr_sql_fn" > /tmp/f
+            # removing the odd characters
+            sed 's/\./_/g' /tmp/f > /tmp/f2
+            sed 's/-/_/g' /tmp/f2 > /tmp/f
 
             table="illuminahiseq_rnaseqv2"
             pk_field="exon"
@@ -53,36 +58,64 @@ do
             fi
 
             schema_table="$name"_"$ctype"."$table"
+            # echo $schema_table >> /tmp/flo
+
             header="CREATE TABLE ""$schema_table""("
 
             echo $header > /tmp/f2
             cat /tmp/f2 /tmp/f > /tmp/stmt
             
-            # removing the odd characters
-            sed 's/\./_/g' /tmp/stmt > /tmp/stmt2
-            sed 's/-/_/g' /tmp/stmt2 > /tmp/stmt
             
             # fixing the table declarations start
+
+            # schema statement start
+            echo "CREATE SCHEMA ""$name"_"$ctype"";" > /tmp/schema_stmt
+            echo "GRANT USAGE ON SCHEMA ""$name"_"$ctype"" TO maplabr;" >> /tmp/schema_stmt
+            echo "ALTER DEFAULT PRIVILEGES IN SCHEMA ""$name"_"$ctype" >> /tmp/schema_stmt
+            echo " GRANT SELECT ON TABLES TO maplabr;" >> /tmp/schema_stmt
+
+<<EOF            
+
+            PGPASSWORD="$PASS" psql -h $HOST -p $PORT -U $USER \
+            -d $DB -t -A -F"," -f /tmp/schema_stmt
+EOF
+
+cat /tmp/schema_stmt
+            # schema statement end
 
             # PGPASSWORD="$PASS" psql -h $HOST -p $PORT -U $USER \
             # -d $DB -t -A -F"," -f "$curr_sql_fn"
 
+<<EOF
+
+
             PGPASSWORD="$PASS" psql -h $HOST -p $PORT -U $USER \
             -d $DB -t -A -F"," -f /tmp/stmt
-
-            rm /tmp/stmt /tmp/stmt2 /tmp/f /tmp/f2
+EOF
+ cat /tmp/stmt
+            rm /tmp/stmt  /tmp/f /tmp/f2
             # PGPASSWORD="$PASS" psql -h $HOST -p $PORT -U $USER \
             # -d $DB -t -A -F"," -c "\copy thca_tumor.humanmethylation450 FROM '/imppc/labs/maplab/share/anna2izaskun/db_region_profile_data/thca_tumor/thca_humanmethylation450_to_SQL.csv' WITH NULL AS 'NA' DELIMITER ','"
 
             # primary key generation start
 
-            echo "CREATE UNIQUE INDEX ""$table"_idx ON "$schema_table"" (""$pk_field"");" > /tmp/stmt_pk
+            echo "\copy ""$schema_table"" from ""$curr_data_fn""  WITH null AS 'NA' DELIMITER ','" \
+                >> /tmp/stmt_pk
+                       
+            echo "CREATE UNIQUE INDEX ""$table"_idx ON "$schema_table"" (""$pk_field"");" >> /tmp/stmt_pk
             echo "ALTER TABLE ""$schema_table" >> /tmp/stmt_pk
             echo " ADD CONSTRAINT ""$table""_pkey PRIMARY KEY USING INDEX ""$table""_idx;" >> /tmp/stmt_pk
+<<EOF
 
+            PGPASSWORD="$PASS" psql -h $HOST -p $PORT -U $USER \
+            -d $DB -t -A -F"," -f /tmp/stmt_pk
+EOF
             # primary key generation end
 
+
+            cat /tmp/stmt_pk
             rm /tmp/stmt_pk
+
             
         done
     done
