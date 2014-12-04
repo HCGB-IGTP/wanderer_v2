0722897aa598fd02372ee26741c75f3225f59e54
Izaskun Mallona <imallona@imppc.org>
2014-12-04 08:45:30 +0100

    Starting with the rendered HTML document as UI
    Minor permission changes

--- run_shiny.sh
+++ run_shiny.sh
@@ -3,4 +3,4 @@
 R3=/soft/general/R-3.1.0/bin/R
 #$R3 -e "shiny::runApp('/imppc/labs/maplab/adiez/region_profile/web/', launch.browser = TRUE, port = 5559)"
 
-$R3 -e "shiny::runApp('/imppc/labs/maplab/imallona/src/regional_profiler/wanderer', launch.browser = TRUE, port = 5559)"
+$R3 -e "shiny::runApp('/imppc/labs/maplab/imallona/src/regional_profiler/wanderer', launch.browser = TRUE, port = 5558)"
