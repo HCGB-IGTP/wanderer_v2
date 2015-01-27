5807aa2bcfa0ce5a8ac05e56d0825543382c9692
Izaskun Mallona <imallona@imppc.org>
2015-01-27 13:32:30 +0100

    Symlinking duplicated code (wanderer_api and wanderer)

--- wanderer_api/GeneSize.R
+++ wanderer_api/GeneSize.R
@@ -1,17 +1,1 @@
-#!/usr/bin/env R
-#
-# @package wanderer
-# @author Anna Diez
-
-GeneSize <- function(con, geneName, geneNamesType){
-  
-  gene <- dbSendQuery(con, statement = paste0("select * from annotations.reduced_genome where ", geneNamesType, " = '", geneName, "'"))
-  gene <- fetch(gene, n = -1)
-  
-  if(dim(gene)[1] == 0) return(list(0))
-  if(dim(gene)[1] > 1) return(list(1))
-  
-  if(dim(gene)[1] == 1) return(list(gene$gene_start, gene$gene_end))
-}
-
-
+/imppc/labs/maplab/imallona/src/regional_profiler/wanderer/GeneSize.R
--- wanderer_api/max_sample.R
+++ wanderer_api/max_sample.R
@@ -1,11 +1,1 @@
-
-max_sample <- function(sample_size, DataType, tissue){
-  
-  sample_size<-sample_size[sample_size[,3]==tissue,]
-  
-  if(DataType=="methylation") sample_size <- as.numeric(sample_size[,4:5])
-  if(DataType=="expression") sample_size <- as.numeric(sample_size[,6:7])
-  
-  return(sample_size)
-}
-  
+/imppc/labs/maplab/imallona/src/regional_profiler/wanderer/max_sample.R
