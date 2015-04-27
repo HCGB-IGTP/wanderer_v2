8195d4fd06640c74120827a8d20935b0ce008fb1
Anna Diez <adiez@imppc.org>
2015-04-27 15:54:48 +0200

    Trying to fix downloads when N=1

--- wanderer/RNAseq_data_table.R
+++ wanderer/RNAseq_data_table.R
@@ -2,8 +2,11 @@
 RNAseq_data_table <- function(ddGene, ddExons){
   
   order_columns <- match(colnames(ddExons)[-1], colnames(ddGene)[-1])
-  dataGene <- ddGene[,-1]
-  dataGene <- data.frame(GENE=ddGene[,1],dataGene[,order_columns])
+  nams<-colnames(ddGene)[-1]
+  nams<-nams[order_columns]
+  dataGene <- as.data.frame(ddGene[,-1])
+  dataGene <- data.frame(ddGene[,1],dataGene[,order_columns])
+  colnames(dataGene)<-c("GENE",nams)
   for(i in 1:6) colnames(dataGene) <- sub("\\.","-",colnames(dataGene))
   dataGene <- t(dataGene)
   dataGene <- data.frame(rownames(dataGene), dataGene[,1])
