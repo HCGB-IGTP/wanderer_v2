f021b394ae70caa81d3391f17ceb941f0d37da0a
Izaskun Mallona <imallona@imppc.org>
2015-05-26 12:03:07 +0200

    External links only for those datasets included

--- wanderer/permalink_modal.R
+++ wanderer/permalink_modal.R
@@ -81,28 +81,31 @@ pop_modal_plot <- function(url) {
 '
 }
 
-
-regulome_explorer_datasets <- list('acc'  = 'acc_2015_03_31',
-                                   'blca' = 'blca_20may13_test',
-                                   'brca' = 'brca_manuscript_rerun_nov12d_pw',
-                                   'chol' = 'chol_20150515_private',
-                                   'coad' = 'coad_03feb13_seq_tumor_only',
-                                   'gbm'  = 'gbm_2013_pub_tumor_only',
-                                   'hnsc' = 'hnsc_03feb13_seq_tumor_only',
-                                   'kirc' = 'kirc_01oct12_A_pw',
-                                   'lgg'  = 'lgg_04oct13_seq',
-                                   'luad' = 'luad_03feb13_seq_tumor_only',
-                                   'lusc' = 'lusc_03feb13_seq_tumor_only',
-                                   'ov'   = 'ov_03feb13_ary_tumor_only',
-                                   'skcm' = 'skcm_01apr14_331_all',
-                                   'stad' = 'stad_23jan14_seq_tumor_only',
-                                   'thca' = 'thca_18oct14_TP',
-                                   'ucec' = 'ucec_28jun13b_seq_tumor_only',
-                                   'uvm'  = 'uvm_20150515_private')
+## selected regulome explorer datasets May 2015
+regulome_explorer_datasets <- function() {
+    dat <- list('acc'  = 'acc_2015_03_31',
+                'blca' = 'blca_20may13_test',
+                'brca' = 'brca_manuscript_rerun_nov12d_pw',
+                'chol' = 'chol_20150515_private',
+                'coad' = 'coad_03feb13_seq_tumor_only',
+                'gbm'  = 'gbm_2013_pub_tumor_only',
+                'hnsc' = 'hnsc_03feb13_seq_tumor_only',
+                'kirc' = 'kirc_01oct12_A_pw',
+                'lgg'  = 'lgg_04oct13_seq',
+                'luad' = 'luad_03feb13_seq_tumor_only',
+                'lusc' = 'lusc_03feb13_seq_tumor_only',
+                'ov'   = 'ov_03feb13_ary_tumor_only',
+                'skcm' = 'skcm_01apr14_331_all',
+                'stad' = 'stad_23jan14_seq_tumor_only',
+                'thca' = 'thca_18oct14_TP',
+                'ucec' = 'ucec_28jun13b_seq_tumor_only',
+                'uvm'  = 'uvm_20150515_private')
+    return(dat)
+}
  
 ## generates an url with an start, end and chromosome for hg19
 generate_regulome_explorer_link <- function(dataset, gene) {
-    dataset <- regulome_explorer_datasets[[dataset]]
+    dataset <- regulome_explorer_datasets()[[dataset]]
     
     ## 'http://explorer.cancerregulome.org/all_pairs/?dataset=chol_20150515_private&t_type=*&t_label=TP53'
     base <- 'http://explorer.cancerregulome.org/all_pairs/?dataset=%s&t_type=*&t_label=%s'
@@ -117,35 +120,39 @@ generate_regulome_explorer_link <- function(dataset, gene) {
 }
 
 
-cbioportal_datasets <- list(acc = 'acc_tcga',
-                            blca = 'blca_tcga_pub',
-                            brca = 'brca_tcga_pub',
-                            cesc = 'cesc_tcga',                            
-                            chol = 'chol_nccs_2013',
-                            coad = 'coadread_tcga',
-                            dlbc = 'dlbc_tcga',
-                            esca = 'esca_tcga',
-                            gbm = 'gbm_tcga_pub2013',
-                            hnsc = 'hnsc_tcga',
-                            kich = 'kich_tcga_pub',
-                            kirc = 'kirc_tcga_pub',
-                            kirp = 'kirp_tcga',
-                            laml = 'laml_tcga_pub',
-                            lgg = 'lgg_tcga',
-                            lihc = 'lihc_tcga',
-                            luad = 'luad_tcga_pub',
-                            lusc = 'lusc_tcga',
-                            ov = 'ov_tcga_pub',
-                            paad = 'paad_tcga',
-                            pcpg = 'pcpg_tcga',
-                            prad = 'prad_tcga', 
-                            read = 'coadread_tcga',
-                            sarc = 'sarc_tcga',
-                            skcm = 'skcm_tcga',
-                            stad = 'stad_tcga',
-                            thca = 'thca_tcga',
-                            ucec = 'ucec_tcga',
-                            ucs = 'ucs_tcga')
+## selected cbioportal datasets May 2015
+cbioportal_datasets <- function(x){
+    dat <- list(acc = 'acc_tcga',
+                blca = 'blca_tcga_pub',
+                brca = 'brca_tcga_pub',
+                cesc = 'cesc_tcga',                            
+                chol = 'chol_nccs_2013',
+                coad = 'coadread_tcga',
+                dlbc = 'dlbc_tcga',
+                esca = 'esca_tcga',
+                gbm = 'gbm_tcga_pub2013',
+                hnsc = 'hnsc_tcga',
+                kich = 'kich_tcga_pub',
+                kirc = 'kirc_tcga_pub',
+                kirp = 'kirp_tcga',
+                laml = 'laml_tcga_pub',
+                lgg = 'lgg_tcga',
+                lihc = 'lihc_tcga',
+                luad = 'luad_tcga_pub',
+                lusc = 'lusc_tcga',
+                ov = 'ov_tcga_pub',
+                paad = 'paad_tcga',
+                pcpg = 'pcpg_tcga',
+                prad = 'prad_tcga', 
+                read = 'coadread_tcga',
+                sarc = 'sarc_tcga',
+                skcm = 'skcm_tcga',
+                stad = 'stad_tcga',
+                thca = 'thca_tcga',
+                ucec = 'ucec_tcga',
+                ucs = 'ucs_tcga')
+    return(dat)
+}
 
 
 
@@ -153,7 +160,7 @@ cbioportal_datasets <- list(acc = 'acc_tcga',
 
 ## http://www.cbioportal.org/index.do?cancer_study_list=coadread_tcga_pub&cancer_study_id=coadread_tcga_pub&genetic_profile_ids_PROFILE_MUTATION_EXTENDED=coadread_tcga_pub_mutations&Z_SCORE_THRESHOLD=2.0&data_priority=0&case_set_id=coadread_tcga_pub_sequenced&case_ids=&gene_set_choice=user-defined-list&gene_list=HDAC9&clinical_param_selection=null&tab_index=tab_visualize&Action=Submit
 generate_cbioportal_link <- function(dataset, gene) {
-    dataset <- cbioportal_datasets[[dataset]]
+    dataset <- cbioportal_datasets()[[dataset]]
 
         ## 'http://explorer.cancerregulome.org/all_pairs/?dataset=chol_20150515_private&t_type=*&t_label=TP53'
     base <- 'http://www.cbioportal.org/index.do?cancer_study_list=%s&cancer_study_id=%s&genetic_profile_ids_PROFILE_MUTATION_EXTENDED=%s_mutations&genetic_profile_ids_PROFILE_COPY_NUMBER_ALTERATION=%s_gistic&Z_SCORE_THRESHOLD=2.0&RPPA_SCORE_THRESHOLD=2.0&data_priority=0&case_set_id=%s_cnaseq&case_ids=&gene_set_choice=user-defined-list&gene_list=%s&clinical_param_selection=null&tab_index=tab_visualize&Action=Submit'
--- wanderer/server.R
+++ wanderer/server.R
@@ -589,28 +589,32 @@ shinyServer(function(input, output, session){
     
     generate_modal(url)
   })
-  
+
+  ## external link to genome browser
   output$genome_browser <- renderText({
     generate_genome_browser_link(chromosome = geneSize()[[3]],
                                  start = input$Zoom[1],
                                  end = input$Zoom[2])    
   })
 
+  ## external link to cbioportal
   output$cbioportal <- renderText({
       if (!is.null(input$Gene) & !is.null(input$TissueType) & !is.null(input$DataType)) {
-          generate_cbioportal_link(dataset = input$TissueType,
-                                   ## gene = input$Gene)
-                                   gene = gene_symbol())
+          if (tolower(input$TissueType) %in% names(cbioportal_datasets())) {
+              generate_cbioportal_link(dataset = input$TissueType,
+                                       gene = gene_symbol())
+          }
       }
   })
 
+  ## external link to regulome explorer
   output$regulome_explorer <- renderText({
       if (!is.null(input$Gene) & !is.null(input$TissueType) & !is.null(input$DataType)) {
-          generate_regulome_explorer_link(dataset = input$TissueType,
-                                          ## gene = input$Gene),
-                                          gene = gene_symbol())
-
-      }
+          if (tolower(input$TissueType) %in% names(regulome_explorer_datasets())) {
+              generate_regulome_explorer_link(dataset = input$TissueType,
+                                              gene = gene_symbol())
+          }
+      }          
   })
 
   
